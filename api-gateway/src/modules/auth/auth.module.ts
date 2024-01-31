a87927debf2bf674dd2b84ba57868fbe4f74f13c
Quang Minh <55976100+QuangMinh98@users.noreply.github.com>
2024-01-31 09:40:31 +0700

    Remove unuse file

--- api-gateway/src/modules/auth/auth.module.ts
+++ api-gateway/src/modules/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MessageBrokerModule } from 'src/MessageBroker/MessageBroker.module';
 import { JwtStrategy } from './strategies/JwtStrategy';
@@ -7,6 +6,6 @@ import { JwtStrategy } from './strategies/JwtStrategy';
 @Module({
   imports: [MessageBrokerModule.register('AUTH_SERVICE')],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
